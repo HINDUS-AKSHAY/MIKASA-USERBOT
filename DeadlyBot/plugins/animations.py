8ae52c01fc7f81b9991b1384c7772be65c821d78
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-08-24 13:22:14 +0530

    Update animations.py

--- DeadlyBot/plugins/animations.py
+++ DeadlyBot/plugins/animations.py
@@ -3,6 +3,22 @@ from collections import deque
 
 from . import *
 
+
+@bot.on(deadly_cmd(pattern=f"fuck$", outgoing=True))
+@bot.on(sudo_cmd(pattern=f"fuck$", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 1
+    animation_ttl = range(0, 101)
+    await eor(event, "fuk")
+    animation_chars = ["👉       ✊️", "👉     ✊️", "👉  ✊️", "👉✊️💦"]
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+        await event.edit(animation_chars[i % 4])
+
+
+
 @bot.on(deadly_cmd(pattern=r"boxs$", outgoing=True))
 @bot.on(sudo_cmd(pattern=r"boxs$", allow_sudo=True))
 async def _(event):
