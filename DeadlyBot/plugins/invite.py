73788ee97532ecc67609085b6455efaf76102f68
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-08-24 17:40:27 +0530

    Update invite.py

--- DeadlyBot/plugins/invite.py
+++ DeadlyBot/plugins/invite.py
@@ -7,7 +7,9 @@ from telethon.tl import functions
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest
 
-from . import *
+from userbot import CMD_HELP
+from DeadlyBot.utils import deadly_cmd, edit_or_reply, sudo_cmd
+from userbot.cmdhelp import CmdHelp
 
 async def get_chatinfo(event):
     chat = event.pattern_match.group(1)
@@ -53,79 +55,67 @@ def user_full_name(user):
     return full_name
 
 
-@bot.on(deadly_cmd(pattern=r"inviteall ?(.*)"))
-@bot.on(sudo_cmd(pattern=r"inviteall ?(.*)", allow_sudo=True))
+@bot.on(deadly_cmd(pattern="inviteall ?(.*)"))
+@bot.on(sudo_cmd(pattern="inviteall ?(.*)", allow_sudo=True))
 async def get_users(event):
     sender = await event.get_sender()
     me = await event.client.get_me()
     if not sender.id == me.id:
-        deadly = await eor(event, "`processing...`")
+        mafia = await edit_or_reply(event, "`processing...`")
     else:
-        deadly = await eor(event, "`processing...`")
-    he_ll = event.pattern_match.group(1)
-    if he_ll == "@deadly_kaal_support":
-        return await deadly.edit("Restricted to invite users from there.")
-    elif he_ll == "@deadly_kaal_support":
-        return await deadly.edit("Restricted to invite users from there.")
-    elif he_ll == "@HELLBOT_CHAT":
-        return await deadly.edit("Restricted to invite users from there.")
+        mafia = await edit_or_reply(event, "`processing...`")
     kraken = await get_chatinfo(event)
     chat = await event.get_chat()
     if event.is_private:
-        return await deadly.edit("`Sorry, Cant add users here`")
+        return await mafia.edit("`Sorry, Cant kidnape users here`")
     s = 0
     f = 0
     error = "None"
 
-    await deadly.edit("**INVITING USERS !!**")
+    await mafia.edit("**TerminalStatus**\n\n`kidnapeing Users.......`")
     async for user in event.client.iter_participants(kraken.full_chat.id):
         try:
             if error.startswith("Too"):
-                return await deadly.edit(
-                    f"**INVITING FINISHED !**\n\n**Error :** \n`{error}`\n\n**Invited :**  `{s}` users. \n**Failed to Invite :** `{f}` users."
+                return await mafia.edit(
+                    f"**Terminal Finished With Error**\n(`May Got Limit Error from telethon Please try agin Later`)\n**Error** : \n`{error}`\n\n• kidnaped `{s}` people \n• Failed to kidnape `{f}` people"
                 )
             await event.client(
                 functions.channels.InviteToChannelRequest(channel=chat, users=[user.id])
             )
             s = s + 1
-            await deadly.edit(
-                f"**INVITING USERS.. **\n\n**Invited :**  `{s}` users \n**Failed to Invite :**  `{f}` users.\n\n**×Error :**  `{error}`"
+            await mafia.edit(
+                f"**Terminal Running...**\n\n• Invited `{s}` people \n• Failed to Invite `{f}` people\n\n**× LastError:** `{error}`"
             )
         except Exception as e:
             error = str(e)
             f = f + 1
-    return await deadly.edit(
-        f"**INVITING FINISHED** \n\n**Invited :**  `{s}` users \n**Failed :**  `{f}` users."
+    return await mafia.edit(
+        f"**Terminal Finished** \n\n• Successfully kidnaped `{s}` people \n• failed to kidnape `{f}` people"
     )
 
 
-@bot.on(deadly_cmd(pattern=r"add ?(.*)"))
-@bot.on(sudo_cmd(pattern=r"add ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="add ?(.*)"))
+@bot.on(sudo_cmd(pattern="add ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
-    if (
-        "addsudo" in event.raw_text.lower()
-        or "addblacklist" in event.raw_text.lower()
-    ):
-        return
     to_add_users = event.pattern_match.group(1)
     if event.is_private:
-        await eod(event, f"Use `{hl}invite` users to a chat, not to a Private Message")
+        await edit_or_reply(event, "`.add` users to a chat, not to a Private Message")
     else:
         logger.info(to_add_users)
         if not event.is_channel and event.is_group:
             # https://lonamiwebs.github.io/Telethon/methods/messages/add_chat_user.html
             for user_id in to_add_users.split(" "):
                 try:
-                    await bot(
+                    await borg(
                         functions.messages.AddChatUserRequest(
                             chat_id=event.chat_id, user_id=user_id, fwd_limit=1000000
                         )
                     )
                 except Exception as e:
                     await event.reply(str(e))
-            await edit_or_reply(event, "Invited User...")
+            await edit_or_reply(event, "Invited Successfully")
         else:
             # https://lonamiwebs.github.io/Telethon/methods/channels/invite_to_channel.html
             for user_id in to_add_users.split(" "):
@@ -137,15 +127,11 @@ async def _(event):
                     )
                 except Exception as e:
                     await event.reply(str(e))
-            await eod(event, "Added user to the chat..")
+            await edit_or_reply(event, "Added user to the chat....")
 
 
 CmdHelp("invite").add_command(
   "add", "<username/id>", "Adds the given user to the group"
 ).add_command(
   "inviteall", "<group username>", "Scraps user from the targeted group to your group. Basically Kidnapps user from one chat to another"
-).add_info(
-  "Invite them."
-).add_warning(
-  "✅ Harmless Module."
 ).add()
