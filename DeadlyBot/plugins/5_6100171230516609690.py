370c80a307ee7320510f19624d55f34ca78a58f3
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-08-30 13:15:30 +0530

    Add files via upload

---
+++ DeadlyBot/plugins/5_6100171230516609690.py
@@ -0,0 +1,21 @@
+from . import *
+
+@bot.on(deadly_cmd(pattern="spic"))
+async def oho(event):
+  if not event.is_reply:
+    return await event.edit('Reply to a self distructing pic !.!.!')
+  k = await event.get_reply_message()
+  pic = await k.download_media()
+  await bot.send_file(event.chat_id, pic, caption=f"""
+  OwO!! LoL, Destruction Mode Pic Destroyed!!
+  Pic captured By LEGENDRY DEADLY BOT🌚🌝
+  """)
+  await event.delete()
+  
+CmdHelp("Self Destruction").add_command(
+  "spic", "This Command Can Capture The Self Destruction Picturr"
+).add_info(
+  "Capture 🤫 Pic."
+).add_warning(
+  "✅ Harmless Module."
+).add()
