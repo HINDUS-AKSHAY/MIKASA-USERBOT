fd6c9a1f2fc2fd6b1679e05ffb813bce8e4831ab
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-08-30 18:49:27 +0530

    Update Spic.py

--- DeadlyBot/plugins/Spic.py
+++ DeadlyBot/plugins/Spic.py
@@ -7,8 +7,8 @@ async def oho(event):
   k = await event.get_reply_message()
   pic = await k.download_media()
   await bot.send_file(event.chat_id, pic, caption=f"""
-  Oʜᴏ! LOL, Dᴇsᴛʀᴜᴄᴛɪᴏɴ Mᴏᴅᴇ Pɪᴄ Dᴇsᴛʀᴏʏᴇᴅ!\nPɪᴄ Dᴇsᴛʀᴏʏᴇᴅ Bʏ\n\n[✰ ᒪᗴᘜᗴᑎᗪᖇY ᗪᗴᗩᗪᒪY ᗷOT ✰](t.me/deadly_kaal_bot) 
-  """)                                              
+  Oʜᴏ! Lᴏʟ 😂, Dᴇsᴛʀᴜᴄᴛɪᴏɴ Mᴏᴅᴇ Pɪᴄ Dᴇsᴛʀᴏʏᴇᴅ!\n\nPɪᴄ Dᴇsᴛʀᴏʏᴇᴅ Bʏ\n\n[✰ ᒪᗴᘜᗴᑎᗪᖇY ᗪᗴᗩᗪᒪY ᗷOT ✰](t.me/deadly_kaal_bot) 
+  """)                                                            
   await event.delete()
   
 CmdHelp("Self Destruction").add_command(
