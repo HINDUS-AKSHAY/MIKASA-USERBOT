f44102ff1cc1d2ae85f85f35bd4a1bcd6c2efce0
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-12-16 22:25:26 +0530

    Update _helper.py

--- DeadlyBot/plugins/_helper.py
+++ DeadlyBot/plugins/_helper.py
@@ -1,99 +1,87 @@
-from userbot.Config import Config
 import asyncio
-
 import requests
-from telethon import functions
 
-from userbot import CMD_LIST, SUDO_LIST
-from deadlybot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot.Config import Config
+from telethon import functions
+from telethon.errors import ChatSendInlineForbiddenError as noin
+from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot, BotInlineDisabledError as noinline, YouBlockedUserError
+
 from . import *
 
-@bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
-async def _(event):
-    if event.fwd_from:
-        return
-    tgbotusername = Config.TG_BOT_USER_NAME_BF_HER
-    input_str = event.pattern_match.group(1)
-    if tgbotusername is not None or mafia_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@MafiaBot_Support")
-        await results[0].click(
-            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
-        )
-        await event.delete()
-    else:
-        await edit_or_reply(event, ["NO_BOT"])
-    
-        if input_str in CMD_LIST:
-          string = "Commands found in {}:\n".format(input_str)
-          for i in CMD_LIST[input_str]:
-              string += "  " + i
-              string += "\n"
-          await event.edit(string)
-        else:
-          await event.edit(input_str + " is not a valid plugin!")
+msg = f"""
+**⚡ ʟɛɢɛռɖaʀʏ ᴀғ ɦɛʟʟɮօt ⚡**
+  •        [📑 Repo 📑](https://github.com/The-HellBot/HellBot)
+  •        [🚀 Deploy 🚀](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FThe-HellBot%2FHellBot&template=https%3A%2F%2Fgithub.com%2Fthe-hellbot%2Fhellbot)
+  •  ©️ {hell_channel} ™
+"""
+botname = Config.BOT_USERNAME
 
-
-@bot.on(sudo_cmd(allow_sudo=True, pattern="help ?(.*)"))
-async def info(event):
-    if event.fwd_from:
-        return
-    input_str = event.pattern_match.group(1)
-    if input_str == "text":
-        string = (
-            "Total {count} commands found in {plugincount} sudo plugins of MafiaBot\n\n"
-        )
-        mafiacount = 0
-        plugincount = 0
-        for i in sorted(SUDO_LIST):
-            plugincount += 1
-            string += f"{plugincount}) Commands found in Plugin " + i + " are \n"
-            for iter_list in SUDO_LIST[i]:
-                string += "    " + str(iter_list)
-                string += "\n"
-                mafiacount += 1
-            string += "\n"
-        if len(string) > 4095:
-            data = string.format(count=mafiacount, plugincount=plugincount)
-            key = (
-                requests.post(
-                    "https://nekobin.com/api/documents", json={"content": data}
-                )
-                .json()
-                .get("result")
-                .get("key")
-            )
-            url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the MafiaBot are [here]({url})"
-            await event.reply(reply_text, link_preview=False)
-            return
-        await event.reply(
-            string.format(count=mafiacount, plugincount=plugincount), link_preview=False
-        )
-        return
-    if input_str:
-        if input_str in SUDO_LIST:
-            string = "<b>{count} Commands found in plugin {input_str}:</b>\n\n"
-            mafiacount = 0
-            for i in SUDO_LIST[input_str]:
-                string += f"  •  <code>{i}</code>"
-                string += "\n"
-                mafiacount += 1
-            await event.reply(
-                string.format(count=mafiacount, input_str=input_str), parse_mode="HTML"
-            )
-        else:
-            reply = await event.reply(input_str + " is not a valid plugin!")
-            await asyncio.sleep(3)
+@deadly_cmd(pattern="repo$")
+async def repo(event):
+    cids = await client_id(event)
+    ForGo10God, HELL_USER, hell_mention = cids[0], cids[1], cids[2]
+    try:
+        hell = await event.client.inline_query(botname, "repo")
+        await hell[0].click(event.chat_id)
+        if event.sender_id == ForGo10God:
             await event.delete()
-            await reply.delete()
+    except (noin, dedbot):
+        await eor(event, msg)
+
+
+@deadly_cmd(pattern="help$")
+async def _(event):
+    tgbotusername = Config.BOT_USERNAME
+    chat = "@Botfather"
+    if tgbotusername is not None:
+        try:
+            results = await event.client.inline_query(tgbotusername, "hellbot_help")
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
+        except noinline:
+            hell = await eor(event, "**Inline Mode is disabled.** \n__Turning it on, please wait for a minute...__")
+            async with bot.conversation(chat) as conv:
+                try:
+                    first = await conv.send_message("/setinline")
+                    second = await conv.get_response()
+                    third = await conv.send_message(tgbotusername)
+                    fourth = await conv.get_response()
+                    fifth = await conv.send_message(perf)
+                    sixth = await conv.get_response()
+                    await bot.send_read_acknowledge(conv.chat_id)
+                except YouBlockedUserError:
+                    return await hell.edit("Unblock @Botfather first.")
+                await hell.edit(f"**Turned On Inline Mode Successfully.** \n\nDo `{hl}help` again to get the help menu.")
+            await bot.delete_messages(
+                conv.chat_id, [first.id, second.id, third.id, fourth.id, fifth.id, sixth.id]
+            )
     else:
-        string = "<b>Please specify which plugin do you want help for !!\
-            \nNumber of plugins : </b><code>{count}</code>\
-            \n<b>Usage:</b> <code>.help plugin name</code>\n\n"
-        mafiacount = 0
-        for i in sorted(SUDO_LIST):
-            string += "≈ " + f"<code>{str(i)}</code>"
-            string += " "
-            mafiacount += 1
-        await event.reply(string.format(count=mafiacount), parse_mode="HTML")
+        await eor(event, "**⚠️ ERROR !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
+
+
+@deadly_cmd(pattern="plinfo(?:\s|$)([\s\S]*)")
+async def hellbott(event):
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await eor(event, str(CMD_HELP[args]))
+        else:
+            await eod(event, "**⚠️ Error !** \nNeed a module name to show plugin info.")
+    else:
+        string = ""
+        sayfa = [
+            sorted(list(CMD_HELP))[i : i + 5]
+            for i in range(0, len(sorted(list(CMD_HELP))), 5)
+        ]
+
+        for i in sayfa:
+            string += f"`📌 `"
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await eor(event, "Please Specify A Module Name Of Which You Want Info" + "\n\n" + string)
