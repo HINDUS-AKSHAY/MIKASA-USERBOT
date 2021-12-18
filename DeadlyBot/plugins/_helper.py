27991d5ba265f766feea4a41fa3513e2831188cf
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-12-18 10:27:02 +0530

    Update _helper.py

--- DeadlyBot/plugins/_helper.py
+++ DeadlyBot/plugins/_helper.py
@@ -12,7 +12,7 @@ async def _(event):
     tgbotusername = Config.BOT_USERNAME
     input_str = event.pattern_match.group(1)
     if tgbotusername is not None or deadly_input == "text":
-        results = await event.client.inline_query(tgbotusername, "@DEADLY_TECHY")
+        results = await event.client.inline_query(tgbotusername, "@DEADLY_USERBOT")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
