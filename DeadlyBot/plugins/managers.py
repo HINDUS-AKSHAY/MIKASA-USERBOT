3a6e19b0a047bd6fb66a2c07d676b552afb0863a
DEADLY-FIGHTERS <87220607+DEADLY-FIGHTERS@users.noreply.github.com>
2021-12-19 18:04:08 +0530

    Update managers.py

--- DeadlyBot/plugins/managers.py
+++ DeadlyBot/plugins/managers.py
@@ -261,11 +261,11 @@ async def handler(event):
 
 
 CmdHelp("managers").add_command(
-  "ls_local", None, "Gives the list of downloaded medias in your DeadlyBot server."
+  "ls_local", None, "Gives the list of downloaded medias in your Deadly Danav Bot server."
 ).add_command(
-  "ls_root", None, "Gives the list of all files in root directory of DeadlyBot repo."
+  "ls_root", None, "Gives the list of all files in root directory of Deadly Danav Bot repo."
 ).add_command(
-  "ls_saved", None, "Gives the list of all files in Saved directory of your DeadlyBot server"
+  "ls_saved", None, "Gives the list of all files in Saved directory of your Deadly Danav Bot server"
 ).add_command(
   "rnsaved", "from | to", "Renames the file in saved directory"
 ).add_command(
